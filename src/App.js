badc447282566f0ba8a8bb0f0df41d031799fa15
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-18 20:05:49 +0530

    Changes done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {/* Hello This is TEsting of  */}
           Learn React
         </a>
       </header>
