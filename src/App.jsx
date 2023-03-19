22a7a9235053a28935657aa87bf6887955e37015
Harshal Tupe <harshaltupe12@gmail.com>
2023-03-19 22:06:01 +0530

    App.Jsx Setup for Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './Design.css';
+import { HomePage } from './HomePage';
+
+function App() {
+  return (
+    <div className="">
+      {/* All Components Should Be Render Through <HomePage/> Do not Interfere with App.jsx */}
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
