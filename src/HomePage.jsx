af70a9503d7b89ab8d2f0789faf225552219cc0c
Harshal Tupe <harshaltupe12@gmail.com>
2023-06-20 14:06:09 +0530

    recruiters added

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { CollegeRank } from './components/CollegeRank'
 import { Footer } from './components/Footer'
+import { Placement } from './components/Placement'
 
 export const HomePage = () => {
   return (
     <div className="main">
       <CollegeRank/>
+      <Placement/>
       <Footer/>
     </div>
   )
