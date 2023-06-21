eb2b8d82a88fd5f85674c8fd483aa57de603ed00
Harshal Tupe <harshaltupe12@gmail.com>
2023-06-21 15:05:25 +0530

    offer section created

---
+++ src/components/Offer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export const Offer = () => {
+  return (
+    <div>
+        <div className="mgm-offer">
+            <div className="offer-panel h-fit bg-[#3C6997] grid grid-cols-1 m-2 text-white rounded-lg md:grid-cols-4">
+
+                <div className="item p-4 ">
+                    <h1 className="text-center text-4xl pb-2">100's</h1>
+                    <div className="text-xs md:text-sm">
+                        <p>of Alumni working in MNCs across the world taking MGMs footprints over the globe.</p>
+                    </div>
+                </div>
+
+                <div className="item p-4">
+                    <h1 className="text-center text-4xl pb-2">50+</h1>
+                    <div className="text-xs md:text-sm">
+                        <p>Recruiters including J P Morgan, Dell, HCL, Infosys, TCS, Wipro and many more have hired MGM students.</p>
+                    </div>
+                </div>
+
+                <div className="item p-4">
+                    <h1 className="text-center text-4xl pb-2">50+</h1>
+                    <div className="text-xs md:text-sm">
+                        <p>Companies who recruit from IITs, NITs and IIMs also recruit from LPU.</p>
+                    </div>
+                </div>
+
+                <div className="item p-4">
+                    <h1 className="text-center text-4xl pb-2">20 LPA</h1>
+                    <div className="text-xs md:text-sm">
+                        <p>MGM pass-out student working in Share Chat with package of Rs 20 LPA in Bengluru.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
