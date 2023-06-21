0324a9b67ba4db1f1f4586a1929da82d06332d49
Harshal Tupe <harshaltupe12@gmail.com>
2023-06-21 15:05:04 +0530

    Component testing

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { CollegeRank } from './components/CollegeRank'
 import { Footer } from './components/Footer'
 import { Placement } from './components/Placement'
+import { Offer } from './components/Offer'
+// import { Slider } from './components/Slider'
 
 export const HomePage = () => {
   return (
     <div className="main">
+      {/* <Slider/> */}
+      <Offer/>
       <CollegeRank/>
       <Placement/>
       <Footer/>
