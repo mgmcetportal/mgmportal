7b0aa4994cf05d1dcfa91e339374d8bf619494ec
Harshal Tupe <harshaltupe12@gmail.com>
2023-06-28 19:58:52 +0530

    Component Rendered

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,18 +3,20 @@ import { CollegeRank } from './components/CollegeRank'
 import { Footer } from './components/Footer'
 import { Placement } from './components/Placement'
 import { Offer } from './components/Offer'
-import { FeedBack } from './components/FeedBack'
-// import { Slider } from './components/Slider'
+// import { FeedBack } from './components/FeedBack'
+import { Slider } from './components/Slider'
+import { VisionMission } from './components/VisionMission'
 
 export const HomePage = () => {
   return (
     <div className="main">
-      {/* <Slider/> */}
-      {/* <Offer/>
+      <Slider/> 
+      <Offer/>
       <CollegeRank/>
       <Placement/>
-      <Footer/> */}
-      <FeedBack/>
+      <VisionMission/>
+      <Footer/> 
+      {/* <FeedBack/> */}
     </div>
   )
 }
